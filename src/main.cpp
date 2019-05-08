e4b85f9c02d84e8c99539e8bee0e5cf9793a8cce
Simon Deng <simonsdeng@gmail.com>
2019-05-07 17:58:38 -0700

    Add GLM headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
-﻿#include <glad/glad.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <vector>
